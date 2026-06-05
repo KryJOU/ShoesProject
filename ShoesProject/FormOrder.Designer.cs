aa675e4f8ee43d797f5f5be1ce6199d56ad4ccad
KryJOU <saveliy.vorotnikov@mail.ru>
2026-06-05 17:06:37 +0500

    Reapply "Готовая система выбора форм"
    This reverts commit 6e7280753d0aeefad2f041f1c1242232f4836018.

---
+++ ShoesProject/FormOrder.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ShoesProject
+{
+    partial class FormOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            panelTop = new Panel();
+            lblUserName = new Label();
+            btn_logout = new Button();
+            dgvProducts = new DataGridView();
+            panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(lblUserName);
+            panelTop.Controls.Add(btn_logout);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(0, 0, 0, 10);
+            panelTop.Size = new Size(982, 40);
+            panelTop.TabIndex = 3;
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Dock = DockStyle.Right;
+            lblUserName.Location = new Point(756, 0);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(50, 20);
+            lblUserName.TabIndex = 6;
+            lblUserName.Text = "label1";
+            lblUserName.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // btn_logout
+            // 
+            btn_logout.BackColor = Color.MediumSpringGreen;
+            btn_logout.Dock = DockStyle.Right;
+            btn_logout.FlatAppearance.BorderSize = 0;
+            btn_logout.FlatStyle = FlatStyle.Flat;
+            btn_logout.Location = new Point(806, 0);
+            btn_logout.Name = "btn_logout";
+            btn_logout.Size = new Size(176, 30);
+            btn_logout.TabIndex = 5;
+            btn_logout.Text = "Выйти";
+            btn_logout.UseVisualStyleBackColor = false;
+            btn_logout.Click += btn_logout_Click;
+            // 
+            // dgvProducts
+            // 
+            dgvProducts.AllowUserToAddRows = false;
+            dgvProducts.AllowUserToDeleteRows = false;
+            dgvProducts.Anchor = AnchorStyles.Left;
+            dgvProducts.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvProducts.BackgroundColor = Color.White;
+            dgvProducts.BorderStyle = BorderStyle.None;
+            dgvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvProducts.DefaultCellStyle = dataGridViewCellStyle1;
+            dgvProducts.Location = new Point(0, 46);
+            dgvProducts.MultiSelect = false;
+            dgvProducts.Name = "dgvProducts";
+            dgvProducts.ReadOnly = true;
+            dgvProducts.RowHeadersVisible = false;
+            dgvProducts.RowHeadersWidth = 51;
+            dgvProducts.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvProducts.Size = new Size(982, 607);
+            dgvProducts.TabIndex = 4;
+            dgvProducts.CellContentClick += dgvProducts_CellContentClick;
+            // 
+            // FormOrder
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(982, 653);
+            Controls.Add(dgvProducts);
+            Controls.Add(panelTop);
+            Name = "FormOrder";
+            Text = "FormOrder";
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvProducts).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private Label lblUserName;
+        private Button btn_logout;
+        private DataGridView dgvProducts;
+    }
+}
--- ShoesProject/Program.cs
+++ ShoesProject/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace ShoesProject
 {
     internal static class Program
@@ -5,30 +8,46 @@ namespace ShoesProject
         [STAThread]
         static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
             bool exitProgram = false;
 
             while (!exitProgram)
             {
                 using (var formLogin = new FormLogin())
                 {
-                    if (formLogin.ShowDialog() == DialogResult.OK)
+                    if (formLogin.ShowDialog() != DialogResult.OK)
                     {
-                        using (var formProducts = new FormProducts(
-                            formLogin.CurrentUser,
-                            formLogin.IsGuest))
+                        exitProgram = true;
+                        break;
+                    }
+
+                    using (var chooseForm = new ChooseForm())
+                    {
+                        DialogResult chooseResult = chooseForm.ShowDialog();
+
+                        if (chooseResult == DialogResult.Cancel)
                         {
-                            if (formProducts.ShowDialog() == DialogResult.Cancel)
+                            continue;
+                        }
+
+                        if (chooseResult == DialogResult.OK)
+                        {
+                            if (chooseForm.Choose == 2)
                             {
+                                using (var formProducts = new FormProducts(formLogin.CurrentUser, formLogin.IsGuest))
+                                {
+                                    formProducts.ShowDialog();
+                                    continue;
+                                }
+                            }
+                            if (chooseForm.Choose == 1)
+                            {
+                                MessageBox.Show("Тут форма заказов");
                                 continue;
                             }
-                            else
-                            {
-                                exitProgram = true;
-                            }
                         }
-                    }
-                    else
-                    {
                         exitProgram = true;
                     }
                 }
